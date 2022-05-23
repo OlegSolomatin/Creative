f056e3acd407f2a4690235acd96d45420909cc1c
OlegSolomatin <1xappleoleg@gmail.com>
2022-05-24 00:20:32 +0300

    Add clock for JS. Update BG app icons. Add signal, battery.

--- Creative 0.0.3/JS/index.js
+++ Creative 0.0.3/JS/index.js
@@ -1,8 +1,8 @@
-let date = new Date(),
-    hou = date.getHours(),
-    min = date.getMinutes(),
-    sec = date.getSeconds();
-
-setInterval(function (){
-    console.log(hou+':'+min+':'+sec);
-}, 1000);
+function clock() {
+    let date = new Date(),
+        hou = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+        min = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+    document.getElementById('clock').innerHTML = hou+':'+min;
+}
+setInterval(clock, 10000);
+clock();

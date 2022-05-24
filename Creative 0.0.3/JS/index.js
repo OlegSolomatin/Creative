2380ff1f78bee0343fe73a5edaa7bd24cde6dcdb
OlegSolomatin <1xappleoleg@gmail.com>
2022-05-25 00:55:45 +0300

    Add day for JS. Update BG app icons. Add reminders and calendar icon.

--- Creative 0.0.3/JS/index.js
+++ Creative 0.0.3/JS/index.js
@@ -6,3 +6,27 @@ function clock() {
 }
 setInterval(clock, 10000);
 clock();
+
+function date() {
+    let date = new Date(),
+        dayOfTheWeek = date.getDay(),
+        number = date.getDate();
+    switch (dayOfTheWeek){
+        case 1: document.getElementById('dayOfTheWeek').innerHTML = 'Пон';
+        break;
+        case 2: document.getElementById('dayOfTheWeek').innerHTML = 'Вт';
+        break;
+        case 3: document.getElementById('dayOfTheWeek').innerHTML = 'Ср';
+        break;
+        case 4: document.getElementById('dayOfTheWeek').innerHTML = 'Чт';
+        break;
+        case 5: document.getElementById('dayOfTheWeek').innerHTML = 'Пт';
+        break;
+        case 6: document.getElementById('dayOfTheWeek').innerHTML = 'Суб';
+        break;
+        default : document.getElementById('dayOfTheWeek').innerHTML = 'Вс';
+    }
+    document.getElementById('numberDay').innerHTML = number;
+}
+setInterval(date, 1000000)
+date();
